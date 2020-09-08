4a449113261d12384256a9972ab84ff504aee72f
Sarfaraz Ahammed <syedsarfarazahammed@gmail.com>
2020-09-08 21:34:25 +0530

    Updates

---
+++ precache-manifest.bb6b1dba49b4af85c03af8eabacfacf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27670c82d622d0bb243e7dee9e97d71e",
+    "url": "/sarfarazsyed/myportfolio/index.html"
+  },
+  {
+    "revision": "a0486ae7a5b141ba5ea2",
+    "url": "/sarfarazsyed/myportfolio/static/css/main.98d9eded.chunk.css"
+  },
+  {
+    "revision": "2ab7af4156932e678777",
+    "url": "/sarfarazsyed/myportfolio/static/js/2.c1b4f82e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sarfarazsyed/myportfolio/static/js/2.c1b4f82e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0486ae7a5b141ba5ea2",
+    "url": "/sarfarazsyed/myportfolio/static/js/main.6de1cd01.chunk.js"
+  },
+  {
+    "revision": "3923a5c45655a4df1d63",
+    "url": "/sarfarazsyed/myportfolio/static/js/runtime-main.00082396.js"
+  }
+]);
